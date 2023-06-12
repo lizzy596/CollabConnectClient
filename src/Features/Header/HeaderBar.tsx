916f723de71b913fe36ddb67206d72dbd075b70e
lizzy596 <lizmeek123@gmail.com>
2023-06-12 17:08:59 -0500

    changed header bar length

--- src/Features/Header/HeaderBar.tsx
+++ src/Features/Header/HeaderBar.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HeaderBar: React.FC = () => {
   return (
-    <nav className="bg-black flex justify-between items-center px-4 py-2">
-      <div className="text-white">CollabConnect</div>
-      <div className="text-white">Login</div>
+    <nav className="bg-black flex justify-between items-center px-4 py-8">
+      <div className="text-white text-3xl ">CollabConnect</div>
+      <div className="text-white text-3xl border-white-300 border-1">Login</div>
     </nav>
   );
 };
